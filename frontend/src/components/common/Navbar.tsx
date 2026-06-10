fd453581cf03924606f31f085159f15efa2cf186
Kullanıcı Adı <kullanici@ornek.com>
2026-06-10 16:30:29 +0300

    style: Use PrintAgo.png instead of SVG for logos

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
-            <img src="/PrintAgoLogo.svg" alt="PrintAgo Logo" className="h-12 w-auto object-contain" />
+            <img src="/PrintAgo.png" alt="PrintAgo Logo" className="h-12 w-auto object-contain" />
           </Link>
 
           {/* Desktop Nav */}
